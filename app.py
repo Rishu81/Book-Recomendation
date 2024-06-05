72fc499126d50c31eec2bfe93bafca8ebc62aad4
unknown <Rishabhgupta7981@gmail.com>
2024-06-05 10:30:00 +0530

    init

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import numpy as np
 import streamlit as st
+import pickle
 
-print("hello")
-# st.header("Books Recommender System")
-# print(np.add(1,2))
+st.header("Book Recommender system")
+model = pickle.load(open("artifacts/model.pkl",'rb'))
+book_pivot = pickle.load(open("artifacts/book_pivot.pkl",'rb'))
+books_name = pickle.load(open("artifacts/books_name.pkl",'rb'))
+final_rating = pickle.load(open("artifacts/final_rating.pkl",'rb'))
+
+
+select_books = st.selectbox(
+    "Type or select a book",
+    books_name
+)
